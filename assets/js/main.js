9c159c58e853f0baf708d9cd6b32a1cad533f658
yutinghCW <33119123+yutinghCW@users.noreply.github.com>
2020-03-28 16:44:10 +0800

    婚禮試做

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,62 @@
-$(function () {
+$(function() {
     var width = $(window).width(),
         height = $(window).height();
     new WOW().init();
-    $('#top').click(function () {
+    $('#top').click(function() {
         $(window).scrollTop(0);
     })
     $('body').append('<div class="blank"></div>');
-    $('.ham, .blank').click(function () {
+    $('.ham, .blank').click(function() {
         $('.ham').toggleClass('opened');
         $('body').toggleClass('opened');
         $('nav ul').fadeToggle();
     });
     if (width >= 1024) {
-        $('.half-banner-group').each(function () {
+        $('.half-banner-group').each(function() {
             var txtHeight = $(this).children('.half-txt-banner').outerHeight();
             $(this).children('.half-img-banner').height(height - txtHeight);
         });
     } else {
-        $('.half-txt-banner').each(function () {
+        $('.half-txt-banner').each(function() {
             var btnGroup = $(this).children().children().children('.btn-group'),
                 phrase = $(this).children().children('p');
             $(btnGroup).remove().clone().insertAfter(phrase).addClass('col-12');
         })
     }
-    $('.btn--modal').each(function () {
-        $(this).click(function () {
+    $('.btn--modal').each(function() {
+        var idx = $(this).data('modal');
+        $(this).click(function() {
+            console.log(idx);
             $('body').addClass('modal-open');
-            $('.modal, .modal-backdrop').fadeIn();
+            $('.' + idx + ', .modal-backdrop').fadeIn();
         })
     });
-    $('.modal-backdrop, modal, .modal-dialog, .modal-close').click(function () {
+    var url = window.location.href,
+        id = url.split('id=')[1];
+    console.log(id);
+    if (id != undefined || id != '') {
+        $('body').addClass('modal-open');
+        $('.' + id + ', .modal-backdrop').fadeIn();
+    }
+    $('.modal-backdrop, .modal, .modal-dialog, .modal-close').click(function() {
         $('body').removeClass('modal-open');
         $('.modal, .modal-backdrop').fadeOut();
     });
-    $('.modal-content').click(function (e) {
+    $('.modal-content').click(function(e) {
         e.stopPropagation()
     });
-    $('.collapse-text').each(function () {
+    $('.collapse-text').each(function() {
         if ($(this).outerHeight() >= 220) {
             $(this).addClass('collapse-hide');
             $('<div class="btn-group"><a href="#!" class="btn btn-collapse">顯示更多</a><a href="#!" class="btn btn-hide" style="display: none">收起內容</a></div>').insertAfter(this);
         }
     })
-    $('.btn-collapse').click(function () {
+    $('.btn-collapse').click(function() {
         $(this).hide();
         $(this).siblings('.btn-hide').show();
         $(this).parent().siblings('.collapse-text').removeClass('collapse-hide');
     })
-    $('.btn-hide').click(function () {
+    $('.btn-hide').click(function() {
         $(this).siblings('.btn-collapse').show();
         $(this).hide();
         $(this).parent().siblings('.collapse-text').addClass('collapse-hide');
@@ -78,15 +87,15 @@ $(function () {
         fade: true,
         cssEase: 'linear',
     });
-    $(window).resize(function () {
+    $(window).resize(function() {
         if (width >= 1024) {
-            $('.half-banner-group').each(function () {
+            $('.half-banner-group').each(function() {
                 var txtHeight = $(this).children('.half-txt-banner').outerHeight();
                 $(this).children('.half-img-banner').height(height - txtHeight);
             });
         }
     });
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         var scroll = $(this).scrollTop();
         if (scroll >= height) {
             $('#top').fadeIn();
